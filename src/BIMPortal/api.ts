6de9c0a01363cc901834bc79b90968c08aae06ce
Florian Becker <info@florianbecker.eu>
2025-09-23 12:19:15 +0200

    fixed cors error

--- src/BIMPortal/api.ts
+++ src/BIMPortal/api.ts
@@ -12,12 +12,14 @@ export async function makeBIMPortalRequest<T extends keyof paths, M extends "get
 
     fixedPath = `.${fixedPath ?? path}`;
 
+    let headers: Record<string, string> = {};
+    if (method === "post")
+        headers = Object.assign(headers, { "Content-Type": "application/json" });
+    if (bearer)
+        headers = Object.assign(headers, {"Authorization": "Bearer " + bearer});
+
     const response = await fetch(new URL(fixedPath, apiHost), {
-        method, headers: {
-            "Content-Type": method === "post" ? "application/json" : undefined,
-            authorization: bearer ? `Bearer ${bearer}` : undefined,
-        },
-        body
+        method, headers, body
     })
 
     if (response.status >= 300) {
